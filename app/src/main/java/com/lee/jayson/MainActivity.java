e08230d815d2bfaee22672a15086a86633dfe03c
CaelumLee <feartheheight@gmail.com>
2018-09-08 13:04:56 +0800

    Initial commit

---
+++ app/src/main/java/com/lee/jayson/MainActivity.java
@@ -0,0 +1,15 @@
+package com.lee.jayson;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setContentView(R.layout.relative_layout);
+        setContentView(R.layout.constraint_layout);
+    }
+}
